8d4271f88c25122de26f5d1aac6329f596b244a9
ajamxb <ajalma08@gmail.com>
2013-10-20 01:28:29 -0400

    Created movies.js
    Note: this JavaScript file only loads the movie data and nothing more. The 1983-2012_genres.csv has yet to be added (if we decide to include it--we can always try to manipulate the data from 1983-2012_movies.csv to get the same information).

---
+++ movies.js
@@ -0,0 +1,30 @@
+// Contains the top 25 movies from 1983-2012 
+var moviesDataset;
+
+// Contains genre data for the top 25 movies from 1983-2012
+var genreDataset;
+
+// csv filenames 
+var files = new Array("1983-2012_movies.csv", "1983-2012_genres.csv");
+
+loadData(files[0]);
+
+/*
+ * Prepares the data so it can be manipulated in
+ * d3.js.
+ */ 
+function loadData(filename){
+    d3.csv(filename, function(error, data) {
+        if (error) {
+            console.log(error);
+        }
+        else{
+            console.log(data);  //DEBUG: delete this later...        
+            moviesDataset = data;
+            //generateTitle();
+    		    //generateBubbleGraph();
+    		    //generateLineGraph();
+    		    //generateSidePanel();
+        }
+    });	
+}
